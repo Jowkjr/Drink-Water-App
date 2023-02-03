73a12318e9b490b0afdb4575e515213bbc064768
Robert Kitowski <robertokitowski@gmail.com>
2023-02-03 19:44:23 +0100

    Function update cup

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const smallCups = document.querySelectorAll(".cup-small");
 const liters = document.getElementById(".liters");
-const precentage = document.getElementById(".precentage");
+const precentage = document.getElementById(".percentage");
 const remained = document.getElementById(".remained");
+
+updateBigCup();
 
 smallCups.forEach((cup, idx) => {
   cup.addEventListener("click", () => {
@@ -23,4 +25,17 @@ function highlightCups(idx) {
       cup.classList.remove("full");
     }
   });
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+  const totalCups = smallCups.length;
+
+  if (fullCups === 0) {
+    precentage.style.visibility = "hidden";
+    precentage.style.height = 0;
+  } else {
+    precentage.style.visibility = "visible";
+    precentage.style.height = `${(totalCups / fullCups) * 330}px`;
+  }
 }
